d80d0a0cdd58dba2b3c430f0d6b729b36b1f4eb8
tjdl102306 <tjdl102306@gmail.com>
2019-05-25 10:18:56 +0800

    start: new project

---
+++ src/test.ts
@@ -0,0 +1,15 @@
+export function testF() {
+  interface person {
+    name: string;
+    age: number;
+  }
+
+  let tom: person = {
+    name: 'Tom',
+    age: 25
+  };
+
+  if (tom.age == 25) {
+    console.log(tom.name + 'is 25 years old.');
+  }
+}
